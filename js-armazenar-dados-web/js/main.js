d07efe852ab452b1bb12a1d2dc64c2f3ee7f418f
isaasc <isabellaszcamposs@gmail.com>
2023-08-28 21:50:34 -0300

    feat(arm-data): add logs to see the values

--- js-armazenar-dados-web/js/main.js
+++ js-armazenar-dados-web/js/main.js
@@ -2,4 +2,8 @@ const form = document.getElementById("novoItem");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    console.log(event.target.elemnts['nome'].value);
+    console.log(event.target.elemnts['quantidade'].value);
+
 });
